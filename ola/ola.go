07d60aa5dd6a8a5969ce1d6ec51c96fd38bbdc9c
adrianosantana <ans3@cin.ufpe.br>
2023-01-04 15:50:32 -0300

    feat: caso uma string vazia seja passada ele deve retorna 'olá, Mundo'

--- ola/ola.go
+++ ola/ola.go
@@ -5,6 +5,9 @@ import "fmt"
 const prefixoOlaPortugues = "Olá, "
 
 func ola(nome string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
 	return prefixoOlaPortugues + nome
 }
 func main() {
--- ola/ola_test.go
+++ ola/ola_test.go
@@ -3,11 +3,22 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := ola("Adriano")
-	esperado := "Olá, Adriano"
-
-	if resultado != esperado {
-		t.Errorf("Esperado: %s resultado: %s", esperado, resultado)
+	verificaMensagemCorreta := func(t *testing.T, esperado string, resultado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("Esperado: '%s' resultado: '%s'", esperado, resultado)
+		}
 	}
+	t.Run("Diz olá para as pessoas", func(t *testing.T) {
+		resultado := ola("Adriano")
+		esperado := "Olá, Adriano"
 
+		verificaMensagemCorreta(t, esperado, resultado)
+	})
+
+	t.Run("Diz 'olá, Mundo' se uma string vazia é passada", func(t *testing.T) {
+		resultado := ola("")
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, esperado, resultado)
+	})
 }
