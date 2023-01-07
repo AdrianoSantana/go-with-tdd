f4ad064b23f12e437f63c73e4ca4bb9847e76318
adrianosantana <ans3@cin.ufpe.br>
2023-01-07 06:57:53 -0300

    feat: adicionado metodos para nossas structs

--- perimetros/perimetro.go
+++ perimetros/perimetro.go
@@ -1,14 +1,24 @@
 package perimetros
 
+import "math"
+
 type Retangulo struct {
 	Altura  float64
 	Largura float64
+}
+
+type Circulo struct {
+	Raio float64
 }
 
 func Perimetro(rect Retangulo) (perimetro float64) {
 	return 2 * (rect.Altura + rect.Largura)
 }
 
-func Area(rect Retangulo) float64 {
-	return rect.Altura * rect.Largura
+func (r Retangulo) Area() float64 {
+	return r.Altura * r.Largura
+}
+
+func (c Circulo) Area() float64 {
+	return math.Pi * c.Raio * c.Raio
 }
--- perimetros/retangulo_perimetro_test.go
+++ perimetros/retangulo_perimetro_test.go
@@ -1,6 +1,9 @@
 package perimetros
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimetro(t *testing.T) {
 	verificaTeste := func(t *testing.T, resultado, esperado float64) {
@@ -17,10 +20,24 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	retangulo := Retangulo{12.0, 6.0}
-	resultado := Area(retangulo)
-	esperado := 72.0
-	if resultado != esperado {
-		t.Errorf("Resultado: '%.2f' Esperado: '%.2f'", resultado, esperado)
+	verificaTeste := func(t *testing.T, resultado, esperado float64) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("Resultado: '%.2f' Esperado: '%.2f'", resultado, esperado)
+		}
 	}
+
+	t.Run("Calcula Área de um Retangulo", func(t *testing.T) {
+		retangulo := Retangulo{12.0, 6.0}
+		resultado := retangulo.Area()
+		esperado := 72.0
+		verificaTeste(t, resultado, esperado)
+	})
+
+	t.Run("Calcula área de um Círculo", func(t *testing.T) {
+		circulo := Circulo{5}
+		resultado := circulo.Area()
+		esperado := math.Pi * (circulo.Raio * circulo.Raio)
+		verificaTeste(t, resultado, esperado)
+	})
 }
