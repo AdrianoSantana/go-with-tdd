f0ff07995888aab1b5691a46a0ecdc8b639872f4
adrianosantana <ans3@cin.ufpe.br>
2023-01-07 06:22:56 -0300

    refactor: removido codigo duplicado

--- soma/soma_todo_resto_test.go
+++ soma/soma_todo_resto_test.go
@@ -6,22 +6,24 @@ import (
 )
 
 func TestSomaTodoResto(t *testing.T) {
+	verificaSlices := func(t *testing.T, resultado []int, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("Resultado '%v' Esperado '%v'", resultado, esperado)
+		}
+	}
+
 	t.Run("Soma slices", func(t *testing.T) {
 		result := SomaTodoResto([]int{1, 2}, []int{0, 9})
 		esperado := []int{2, 9}
-
-		if !reflect.DeepEqual(result, esperado) {
-			t.Errorf("Resultado '%v' Esperado '%v'", result, esperado)
-		}
+		verificaSlices(t, result, esperado)
 	})
 
 	t.Run("Soma slices vázios", func(t *testing.T) {
 		result := SomaTodoResto([]int{}, []int{0, 9})
 		esperado := []int{0, 9}
 
-		if !reflect.DeepEqual(result, esperado) {
-			t.Errorf("Resultado '%v' Esperado '%v'", result, esperado)
-		}
+		verificaSlices(t, result, esperado)
 	})
 
 }
