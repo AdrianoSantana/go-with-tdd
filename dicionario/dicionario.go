44c95b8e776ee4cf0193b009402406cdc2193dc3
adrianosantana <ans3@cin.ufpe.br>
2023-01-09 15:30:04 -0300

    feat: tratando caso em que a palavra não existe no dicionario

--- dicionario/dicionario.go
+++ dicionario/dicionario.go
@@ -1,7 +1,13 @@
 package dicionario
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Busca(dicionario map[string]string, palavra string) string {
-	return dicionario[palavra]
+func (d Dictionary) Busca(dicionario map[string]string, palavra string) (string, error) {
+	definicao, existe := dicionario[palavra]
+	if !existe {
+		return "", errors.New("Não foi possivel encontrar uma definição para essa palavra")
+	}
+	return definicao, nil
 }
--- dicionario/dicionario_test.go
+++ dicionario/dicionario_test.go
@@ -4,10 +4,21 @@ import "testing"
 
 func TestDicionario(t *testing.T) {
 	dicionario := Dictionary{"teste": "Isso é apenas um teste"}
+	t.Run("Busca uma palavra existente", func(t *testing.T) {
+		resultado, _ := dicionario.Busca(dicionario, "teste")
+		esperado := "Isso é apenas um teste"
+		comparaString(t, resultado, esperado)
+	})
 
-	resultado := dicionario.Busca(dicionario, "teste")
-	esperado := "Isso é apenas um teste"
-	comparaString(t, resultado, esperado)
+	t.Run("Busca por uma palavra que não existe", func(t *testing.T) {
+		dicionario := Dictionary{}
+		_, err := dicionario.Busca(dicionario, "desconhecida")
+		if err == nil {
+			t.Fatal("Era esperado um erro!")
+		}
+		esperado := "Não foi possivel encontrar uma definição para essa palavra"
+		comparaString(t, err.Error(), esperado)
+	})
 }
 
 func comparaString(t *testing.T, resultado string, esperado string) {
